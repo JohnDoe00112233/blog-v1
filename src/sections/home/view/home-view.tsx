8b01446134c3f7607d5680b47e92a74692ec5757
John Doe <nguyenquoccuong1998x@gmail.com>
2024-08-07 20:31:39 +0700

    first commit

---
+++ src/sections/home/view/home-view.tsx
@@ -0,0 +1,53 @@
+'use client';
+
+import Stack from '@mui/material/Stack';
+
+import { BackToTop } from 'src/components/animate/back-to-top';
+import { ScrollProgress, useScrollProgress } from 'src/components/animate/scroll-progress';
+
+import { HomeHero } from '../home-hero';
+import { HomeFAQs } from '../home-faqs';
+import { HomeMinimal } from '../home-minimal';
+import { HomePricing } from '../home-pricing';
+import { HomeTestimonials } from '../home-testimonials';
+import { HomeIntegrations } from '../home-integrations';
+import { HomeAdvertisement } from '../home-advertisement';
+import { HomeHighlightFeatures } from '../home-highlight-features';
+
+// ----------------------------------------------------------------------
+
+export function HomeView() {
+  const pageProgress = useScrollProgress();
+
+  return (
+    <>
+      <ScrollProgress
+        variant="linear"
+        progress={pageProgress.scrollYProgress}
+        sx={{ position: 'fixed' }}
+      />
+
+      <BackToTop />
+
+      <HomeHero />
+
+      <Stack sx={{ position: 'relative', bgcolor: 'background.default' }}>
+        <HomeMinimal />
+
+        <HomeHighlightFeatures />
+
+        <HomeIntegrations />
+
+        <HomePricing />
+
+        <HomeTestimonials />
+
+        <HomeFAQs />
+
+
+
+        <HomeAdvertisement />
+      </Stack>
+    </>
+  );
+}
