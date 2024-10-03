47b0c64c295c97df7059dffa5821b785ca3655d0
John Doe <nguyenquoccuong1998x@gmail.com>
2024-10-03 23:51:17 +0700

    fix/token

--- src/app/(index)/page.tsx
+++ src/app/(index)/page.tsx
@@ -5,7 +5,7 @@ import { HomeView } from 'src/sections/home/view';
 export const metadata = {
   title: 'K89bet.com | Link vào K89bet chính thức không bị chặn link',
   description:
-    ' for your next project with Minimal UI Kit, built on the newest version of Material-UI ©, ready to be customized to your style',
+    ' next project with Minimal UI Kit, built on the newest version of Material-UI ©, ready to be customized to your style',
 };
 
 export default function Page() {
