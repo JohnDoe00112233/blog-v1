1327895e64f52c9f89b9aa934a647d907338b394
John Doe <nguyenquoccuong1998x@gmail.com>
2024-10-22 13:57:28 +0700

    feat/fix pm2 server

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -11,8 +11,6 @@ module.exports = {
       env_production: {
         NODE_ENV: "production",
       },
-      // Thêm post_deploy để build trước khi start
-      post_deploy: "yarn build",
     },
   ],
 };
